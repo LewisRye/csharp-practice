0f67d8b95aee26db9aa4733c016e5eeeed31d50c
Lewis <lewisryework@gmail.com>
2022-10-05 16:30:44 +0100

    added first week of university array operations
    - array reverse
    - array left shift

---
+++ ArrayReverse/ArrayReverse/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace ArrayReverse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // this program reverses the contents of an array
+
+            Console.Write("How many elements would you like in the array: ");
+            int input = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine();
+            int[] Array = new int[input];
+
+            for (int i = 0; i < input; i++)
+            {
+                Console.Write($"Enter the value of element {i}: ");
+                Array[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            int length = Array.Length;
+            int temp = 0;
+
+            for (int i = 0; i < length / 2; i++)
+            {
+                temp = Array[length - 1];
+                Array[length - 1] = Array[i];
+                Array[i] = temp;
+                length--;
+            }
+
+            Console.Write("\nThe reverse of your array is: ");
+
+            foreach (int i in Array)
+            {
+                Console.Write(i);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ArrayRightShift/ArrayRightShift/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace ArrayRightShift
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // this program does a right shift on a user defined array m times
+
+            Console.Write("How many elements would you like in the array: ");
+            int input = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine();
+            int[] array = new int[input];
+
+            for (int i = 0; i < input; i++)
+            {
+                Console.Write($"Enter the value of element {i}: ");
+                array[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            Console.WriteLine();
+            Console.Write("How many shifts would you like?: ");
+            int m = Convert.ToInt32(Console.ReadLine());
+
+            int length = array.Length;
+            int temp = 0;
+
+            for (int i = 0; i < m; i++)
+            {
+                temp = array[length - 1]; // the final value of the array
+
+                for (int j = 0; j < length - 1; j++)
+                {
+                    array[length - j - 1] = array[length - j - 2];
+                }
+
+                array[0] = temp;
+            }
+
+            Console.Write($"\nYour array shifted right {m} times is: ");
+            foreach (int value in array)
+            {
+                Console.Write(value);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
